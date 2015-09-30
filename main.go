fb202ff7c204383691c34a608d4dfbd700f6a856
Maksimov Sergey <konjoot@gmail.com>
2015-09-30 18:18:37 +0300

    Fix the program output

--- main.go
+++ main.go
@@ -10,11 +10,11 @@ var output string
 
 func init() {
 	flag.Usage = func() {
-		fmt.Printf("Usage: %s -out=out.path<optional> in.path<required>\n\n", os.Args[0])
+		fmt.Printf("Usage: %s [-out=out.path] in.path\n\n", os.Args[0])
 		flag.PrintDefaults()
 	}
 
-	flag.StringVar(&output, "out", "out.go", "Specify a path to the output file (optional)")
+	flag.StringVar(&output, "out", "out.go", "Specify a path to the output file")
 
 	flag.Parse()
 }
@@ -22,7 +22,7 @@ func init() {
 func main() {
 	checkRequirements()
 
-	fmt.Printf("%v, %s", flag.Args(), output)
+	fmt.Printf("input file: %s, output file: %s\n", flag.Arg(0), output)
 }
 
 func checkRequirements() {
@@ -31,10 +31,10 @@ func checkRequirements() {
 	if len(args) == 0 {
 		flag.Usage()
 
-		fmt.Printf("Error: The input file is required\n")
+		fmt.Printf("Error! The input file is required\n")
 
 		os.Exit(1)
 	} else if len(args) > 1 {
-		fmt.Printf("Notice: to many positional arguments, ignoring %v\n", args[1:])
+		fmt.Printf("Notice! To many positional arguments, ignoring %v\n", args[1:])
 	}
 }
